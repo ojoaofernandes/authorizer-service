4f76cb11f3e4d6cfb06f89d63de7c0c004249553
Joao Fernandes <ojoaofernandes@gmail.com>
2021-11-02 20:18:35 -0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello");
+const express = require('express');
+const mapOfProducts = require('./products.json')
+  .reduce((acc, p) => ({ ...acc, [p.id]: p }), {});
+
+  console.log(mapOfProducts)
+
+const app = express();
+app.use(express.json());
+app.post('/checkout', (req, res) => {
+  const { products } = req.body
+  const items = products.map(({ id, quantity }) => ({
+    id,
+    quantity,
+    "unit_amount": mapOfProducts[id].amount,
+    "total_amount": mapOfProducts[id].amount * quantity,
+    discount: 0,
+    "is_gift": false
+  }));
+
+  res.send({
+    "total_amount": items.reduce((acc, i) => acc + i.total_amount, 0),
+    "total_amount_with_discount": items.reduce((acc, i) => acc + i.total_amount, 0),
+    "total_discount": items.reduce((acc, i) => acc + i.discount, 0),
+    "products": items
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`App listening at http://localhost:3000`);
+});
